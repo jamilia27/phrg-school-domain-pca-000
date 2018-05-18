4c66f6ca08ee6b4f9fd1427df89a87a251fd0a68
Katie Weber <k.weber0893@gmail.com>
2018-05-18 09:49:34 -0400

    Created OO School Domain

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_accessor :school_name, :roster
+
+  def initialize(school_name)
+    @roster = {}
+    @school_name = school_name
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_hash = {}
+    @roster.each do |grade, name|
+      name.sort!
+    end
+  end
+end
+
